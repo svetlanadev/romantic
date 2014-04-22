06640eed1dc389dd046eec836d6149248075f2c3
dlyapun <dlyapun@gmail.com>
2014-04-22 13:21:48 +0300

    Master - edit model Hike

--- hike/models.py
+++ hike/models.py
@@ -5,37 +5,28 @@ from django.db import models
 
 
 class Hike(models.Model):
-    STATE_CHOICE = (
-        (1, '1 к.с'),
-        (2, '2 к.с'),
-        (3, '3 к.с'),
-        (4, '4 к.с'),
-        (5, '5 к.с'),
-        (6, '6 к.с'),
-        ('PVD', 'ПВД'),
-        ('Expedition', 'Экспедиция'),
-    )
-
     user = models.OneToOneField(User)
     type_hike = models.ForeignKey('TypeHike')
     creation_date = models.DateTimeField(auto_now_add=True)
     date_start = models.DateField()
     data_finish = models.DateField()
-    difficulty = models.TextField(choices=STATE_CHOICE)
+    state_group = models.ForeignKey('Difficulty')
     requirements = models.TextField()
     state_group = models.ForeignKey('StateGroup')
-    state_group = models.ForeignKey('Region')
+    region = models.ForeignKey('Region')
 
     class Meta:
         ordering = ["-creation_date"]
 
     def __unicode__(self):
-        return self.type_hike
+        return u'%s %s %s' % (self.user.username,
+                              self.type_hike.type_hike,
+                              self.difficulty)
 
 
 # Тип похода - горный, или пеший например
 class TypeHike(models.Model):
-    typehike = models.CharField(max_length=50)
+    type_hike = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.typehike
@@ -55,3 +46,11 @@ class StateGroup(models.Model):
 
     def __unicode__(self):
         return self.state_group
+
+
+# Сложность похода
+class Difficulty(models.Model):
+    difficulty = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.difficulty
