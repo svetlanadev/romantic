cc4f718362459922a57841a95bbee2b40e3adc5b
dlyapun <dlyapun@gmail.com>
2014-05-17 14:22:30 +0300

    blog: Add admin.py

--- force_blog/models.py
+++ force_blog/models.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from django.db import models
-from django.conf.settings import AUTH_PROFILE_MODULE
+from django.conf import settings
 
 
 class BlogPost(models.Model):
@@ -18,12 +18,12 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=50, verbose_name=u'Заголовок')
     date_creation = models.DateTimeField(auto_now_add=True)
     date_publication = models.DateTimeField(blank=True, null=True)
-    owner = models.ForeignKey(AUTH_PROFILE_MODULE, verbose_name=u'Автор')
+    owner = models.OneToOneField(settings.AUTH_PROFILE_MODULE, verbose_name=u'Автор')
     text = models.TextField(verbose_name=u'Страничка')
     state = models.SmallIntegerField(default=DISABLE, choices=STATE_CHOICE)
     category = models.ManyToManyField('Category',
                                       verbose_name=u'Категории',
-                                      related_name="blogposts")
+                                      related_name="blogposts_category")
     files = models.ManyToManyField('AttachedFiles', blank=True, null=True)
 
     class Meta:
@@ -36,7 +36,7 @@ class BlogPost(models.Model):
 
     def __unicode__(self):
         return u'%s %s - %s' % (self.title,
-                                self.owner.username,
+                                self.owner,
                                 self.state,)
 
 
@@ -45,7 +45,11 @@ class AttachedFiles(models.Model):
     one_file = models.FileField(upload_to='files/')
     category = models.ManyToManyField('Category',
                                       verbose_name=u'Категории',
-                                      related_name="blogposts")
+                                      related_name="file_category")
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
 
     def get_absolute_url(self):
         return u'/files/%s' % self.id
@@ -56,6 +60,10 @@ class AttachedFiles(models.Model):
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __unicode__(self):
         return self.category
