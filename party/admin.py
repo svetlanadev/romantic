f0a139cee5e9478dcf98ce9d0d9c169fc0c4eed2
dlyapun <dlyapun@gmail.com>
2014-05-17 14:41:52 +0300

    master: edit party

--- party/admin.py
+++ party/admin.py
@@ -1,14 +1,13 @@
 # coding=utf-8
 
-from party.models import Party, Files
+from party.models import Party
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 
 
 class PartyAdmin(SummernoteModelAdmin):
-    filter_horizontal = ('files',)
-    list_display = ('name', 'date', 'state')
+    filter_horizontal = ('files', 'category')
+    list_display = ('name', 'date_creation', 'state')
 
 
 admin.site.register(Party, PartyAdmin)
-admin.site.register(Files)
--- party/models.py
+++ party/models.py
@@ -1,25 +1,32 @@
 # coding=utf-8
 
 from django.db import models
-from force_blog.models import AttachedFiles
+from force_blog.models import AttachedFiles, Category
 
 
 class Party(models.Model):
     DISABLE = 0
     ENABLE = 1
+    DELETE = 2
 
     STATE_CHOICE = (
         (DISABLE, 'Disable'),
         (ENABLE, 'Enable'),
+        (DELETE, 'Delete'),
     )
 
     name = models.CharField(max_length=50, verbose_name=u'Мероприятие')
     date_creation = models.DateTimeField(auto_now_add=True)
-    date_start = models.DateField(verbose_name=u'Начало похода')
-    data_finish = models.DateField(verbose_name=u'Конец похода')
-    state = models.SmallIntegerField(default=DISABLE, choices=STATE_CHOICE)
+    date_start = models.DateField(verbose_name=u'Начало мероприятия')
+    data_finish = models.DateField(verbose_name=u'Конец мероприятия')
+    state = models.SmallIntegerField(default=DISABLE,
+                                     choices=STATE_CHOICE,
+                                     verbose_name=u'Статус')
     text = models.TextField(verbose_name=u'Страничка')
     files = models.ManyToManyField(AttachedFiles, blank=True, null=True)
+    category = models.ManyToManyField(Category,
+                                      verbose_name=u'Категории',
+                                      related_name="party_category")
 
     class Meta:
         ordering = ["-date_creation"]
