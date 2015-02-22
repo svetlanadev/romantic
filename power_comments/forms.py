2e94082368d96cffa26c824556787d1196465979
Dmitry Lyapun <dlyapun@gmail.com>
2015-02-22 13:14:35 +0200

    add ajax for comments

--- power_comments/forms.py
+++ power_comments/forms.py
@@ -5,6 +5,6 @@ from django import forms
 
 
 class PowerCommentForm(forms.Form):
-    text = forms.CharField(min_length=10,
+    text = forms.CharField(min_length=5,
                            max_length=400,
-                           error_messages={'required': 'Миннимум 10 символов'})
+                           error_messages={'required': 'Миннимум 5 символов'})
--- power_comments/views.py
+++ power_comments/views.py
@@ -1,11 +1,16 @@
 # coding: utf-8
 
+import json
+
 # from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from profile.models import CustomUser
 from django.shortcuts import render_to_response, redirect, render
 from power_comments.models import PowerComment
 from power_comments.forms import PowerCommentForm
+from django.utils import simplejson
+from django.http import HttpResponse
+from random import randint
 
 
 @login_required
@@ -78,3 +83,78 @@ def disable_power_comments(request):
         return redirect(comment.app)
 
 # <django.db.models.fields.related.ManyRelatedManager object at 0x7f50182ede90>
+
+def ajax_test(request):
+    results = {'success':False}
+
+    if request.is_ajax():
+        print request.POST
+    # Тут — потрібні нам алгоритми
+    if True:
+        results = {'success':True, 'param1':'Good', 'param2':randint(0,40)}
+
+    json = simplejson.dumps(results)
+    return HttpResponse(json, mimetype='application/json')
+
+
+def ajax_karma_minus(request):
+    user = CustomUser.objects.get(user=request.user)
+    results = {'success':False}
+
+    # Тут — потрібні нам алгоритми
+    if request.is_ajax():
+        id_comment = request.POST['id_comment']
+        comment = PowerComment.objects.get(id=id_comment)
+        message = ""
+        if user.karma > -10:
+            if user not in comment.karma_users.all():
+                comment.rating = comment.rating - 1
+                user.karma = user.karma - 1
+                comment.save()
+                user.save()
+                comment.karma_users.add(user)
+                message = "Ваш голос учтен"
+            else:
+                message = "Вы уже поставили рейтинг"
+                print message
+        else:
+            message = "Недостаточно кармы для комментирования"
+            print message
+
+        results = {'success':True, 'rating':comment.rating, 'id_comment':comment.id, 'message':message, }
+        
+
+    json = simplejson.dumps(results)
+    return HttpResponse(json, mimetype='application/json')
+
+
+def ajax_karma_plus(request):
+    user = CustomUser.objects.get(user=request.user)
+    results = {'success':False}
+    print user
+
+    # Тут — потрібні нам алгоритми
+    if request.is_ajax():
+        id_comment = request.POST['id_comment']
+        comment = PowerComment.objects.get(id=id_comment)
+        message = ""
+        if user.karma > -10:
+            if user not in comment.karma_users.all():
+                comment.rating = comment.rating + 1
+                user.karma = user.karma + 1
+                comment.save()
+                user.save()
+                comment.karma_users.add(user)
+                message = "Ваш голос учтен"
+            else:
+                message = "Вы уже поставили рейтинг"
+                print message
+        else:
+            message = "Недостаточно кармы для комментирования"
+            print message
+
+        results = {'success':True, 'rating':comment.rating, 'id_comment':comment.id, 'message':message, }
+        
+
+    json = simplejson.dumps(results)
+    return HttpResponse(json, mimetype='application/json')
