3c42945ecb1afd3762b220f05b3f4050633c1dd0
Dmytro Lyapun <dlyapun@gmail.com>
2015-09-25 13:20:30 +0200

    Add party and profile context processors

--- banner/context_processor.py
+++ banner/context_processor.py
@@ -1,23 +1,14 @@
 # coding=utf-8
 
 from banner.models import BannerTitle
-from profile.models import CustomUser
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 
 
 def contex_banner(request):
-    if request.user.is_authenticated():
-        try:
-            profile = CustomUser.objects.get(user=request.user)
-        except ObjectDoesNotExist:
-            profile = request.user
-    else:
-        profile = ""
-
     if request.path == '/party/':
         banner = 'winter.jpg'
     else:
         banner = 'general.jpg'
 
-    return {'banner': banner, 'profile':profile, 'mode_site': settings.DEBUG}
+    return {'banner': banner }
---
+++ profile/context_processor.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+from profile.models import CustomUser
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def contex_profile(request):
+    if request.user.is_authenticated():
+        try:
+            profile = CustomUser.objects.get(user=request.user)
+        except ObjectDoesNotExist:
+            profile = request.user
+    else:
+        profile = ""
+
+    return {'profile': profile}
